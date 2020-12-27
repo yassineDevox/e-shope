7d3468a846e531ef647a84ceb098f5f584fa606a
Yassine Rassy <yassine.rassy1@gmail.com>
2020-12-27 02:34:17 +0100

    validation form done with fix a lot of bugs god damn hhhhh

--- src/components/add-product/addProduct.jsx
+++ src/components/add-product/addProduct.jsx
@@ -7,7 +7,7 @@ export default class ProductADD extends React.Component {
   constructor(props) {
     //passer les attributs (props ) a la classe mere Component
     super(props);
-    //class variable error initialised to true because we dont have data inputs for the first time 
+    //class variable error initialised to true because we dont have data inputs for the first time
     this.ERROR = true;
     //definir les variables qui seront actualiser automatiquement dans la partie html
     // this.didupdate=0;
@@ -31,81 +31,96 @@ export default class ProductADD extends React.Component {
   //partie UI (user interface html )
   render() {
     return (
-        <AddProductUI
-          title={this.state.product.title}
-          desc={this.state.product.desc}
-          img={this.state.product.img}
-          errorTitle={this.state.error.title}
-          errorImg={this.state.error.img}
-          errorDesc={this.state.error.desc}
-          onChangeInput={this.onChangeInput}
-          onAddProduct={this.onAddProduct}
-        />
+      <AddProductUI
+        title={this.state.product.title}
+        desc={this.state.product.desc}
+        img={this.state.product.img}
+        errorTitle={this.state.error.title}
+        errorImg={this.state.error.img}
+        errorDesc={this.state.error.desc}
+        onChangeInput={this.onChangeInput}
+        onAddProduct={this.onAddProduct}
+      />
     );
   }
 
   //e evenement
   onChangeInput = (e) => {
+    let error = {
+      title: false,
+      desc: false,
+      img: false,
+    };
     //e.target how element litra3lih event(onChange)
     let value = e.target.value;
     let name = e.target.name;
     //7at liya lvaleur dyal name fel partie key
-    this.setState((currentState) => {
-      currentState.product[name] = value;
-      //validate the current input
-      this.validateInput(currentState,name);
-      return currentState;
+    this.setState((s) => {
+      s.product[name] = value;
+      return s;
+    });
+
+    this.validateInput(error, name);
+
+    this.setState((s) => {
+      s.error[name] = error[name];
+      return s;
     });
   };
 
   onAddProduct = (event) => {
+    let error = {
+      title: false,
+      desc: false,
+      img: false,
+    };
     //pour que la page ne s'actualise pas apres le click
     event.preventDefault();
 
-    this.setState((currentState) => {
-      //etape 3 changer la valeur de error si title est vide
-      Object.keys(currentState.product).map((key) => {
-        this.ERROR = this.validateInput(currentState,key);
-      });
-      
-    }); 
-      
-    if (!this.ERROR) {
-        //Collect data using spread operator
-        const Data = { ...this.state.product };
-  
-        // console.log('sending data')
-        // send the data to the server using axios
-        axios.post("/products.json", Data).then((data) => {
-          console.log(data);
-        });
-      }
-  
+    Object.keys(error).map((key) => {
+      this.validateInput(error, key);
+    });
 
-    
+    console.log(error);
+
+    this.setState({ error: error });
+
+    // if (!this.ERROR) {
+    //   this.sendData();
+    // }
+  };
+
+  sendData = () => {
+    //Collect data using spread operator
+    const Data = { ...this.state.product };
+
+    // console.log('sending data')
+    // send the data to the server using axios
+    axios.post("/products.json", Data).then((data) => {
+      console.log(data);
+    });
   };
 
   //input pour indiquer les entrers et aussi la partie error lier avec cet input
-  validateInput = (myState,input) => {
-
+  validateInput = (error, input) => {
+    console.log(this.state, input, this.state.product[input]);
     // console.log(this.state,input,this.state.product[input])
     let err = false;
 
-    if (myState.product[input] == "") err = true;
+    if (this.state.product[input] == "") err = true;
     // else this.ERROR=false
-     myState.error[input] = err;
-     this.forceUpdate();
-     return err;
-   
+    error[input] = err;
+    console.log(this.state, input);
+
+    return err;
   };
 
   //life cycle hooks react
-  componentDidMount=() => {
+  componentDidMount = () => {
     // this.setState((currentState) => {
     //   currentState.lifeCycle.didmount++;
     //   return currentState;
     // });
-  }
-  componentDidUpdate=() => {
-  }
+  };
+  componentDidUpdate = () => {};
 }
