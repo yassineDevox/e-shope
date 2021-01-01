62f6663df721f428171036c51bb28a236a7a9448
Yassine Rassy <yassine.rassy1@gmail.com>
2021-01-01 22:32:27 +0100

    product-card created done

---
+++ src/shared/card-product.jsx
@@ -0,0 +1,16 @@
+export default function CardProduct(props) {
+    return (
+      <div className="card" style={{ width: "15rem" }}>
+        <img src={props.img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">
+              {props.desc}
+          </p>
+          <a href="#" className="btn btn-warning text-uppercase">
+            add to cart 
+          </a>
+        </div>
+      </div>
+    );
+  }
